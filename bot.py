32029b767ed770f03e1e96ed162fdb2064955179
Loy van Beek <loy.vanbeek@mojin-robotics.de>
2021-11-11 20:44:23 +0100

    Only send a message if we're overgenerating once a day

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 import configparser
+import datetime
+import logging
 import sys
+import time
 
 import requests
 from telegram.ext import Updater
@@ -16,6 +19,30 @@ class SurplusBot:
 
         self.telegram_updater = Updater(token=self.telegram_api_key,
                                         use_context=True)
+
+        self.check_interval = int(config['Bot']['CheckInterval'])
+
+        self._last_message_sent_date = None
+
+    def is_new_day(self) -> bool:
+        """Return True when it's a new day since the last time a message was sent"""
+        today = datetime.date.today()
+        if self._last_message_sent_date:
+            if self._last_message_sent_date < today:
+                logging.info("It's a new day! The sun could be shining")
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def loop(self):
+        while True:
+            try:
+                self.check_electricity_live()
+                time.sleep(self.check_interval)
+            except KeyboardInterrupt:
+                break
 
 
     def send_message(self, message: str):
@@ -33,15 +60,21 @@ class SurplusBot:
         surplus = returned - delivered
 
         if surplus >= self.surplus_threshold:
-            self.send_message(f'Now generating {returned}W but only using {delivered}W, so you have {surplus}W extra')
+            if self.is_new_day():
+                self.send_message(f'Now generating {returned}W but only using {delivered}W, so you have {surplus}W extra')
+                self._last_message_sent_date = datetime.date.today()
+            else:
+                logging.info("Generating surplus over threshold but already sent a message")
         else:
-            self.send_message('You are using power but not generating a reasonable surplus')
+            logging.info(f'Now generating {returned}W but using {delivered}W, so you have {surplus}W short')
 
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(sys.argv[1])
 
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s]: %(message)s')
+
     bot = SurplusBot(config)
 
-    bot.check_electricity_live()
+    bot.loop()
